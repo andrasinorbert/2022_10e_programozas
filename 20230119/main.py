ea8e9564121603c7886d84db21ffe746aec8fc6a
andrasinorbert92 <113181319+andrasinorbert@users.noreply.github.com>
2023-01-19 12:28:15 +0100

    20230119

--- 20230119/main.py
+++ 20230119/main.py
@@ -2,7 +2,13 @@ lista=[]
 
 f=open("20230119/input.txt")
 for i in f:
-    lista.append(i)
+    #lista.append(int(i))
+    lista.append(int(i.strip()))
 f.close()
 
+tmp=0
+for i in lista:
+    tmp+=i
+
 print(lista)
+print(tmp/len(lista))
