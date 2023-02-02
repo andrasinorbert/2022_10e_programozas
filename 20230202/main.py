f3f64c29696b27614b259fb82e45367c2a32254d
andrasinorbert92 <113181319+andrasinorbert@users.noreply.github.com>
2023-02-02 15:23:34 +0100

    20230202

---
+++ 20230202/main.py
@@ -0,0 +1,15 @@
+f=open("20230202/input.txt", encoding="utf-8")
+
+
+f.close()
+
+
+nevek=[]
+korok=[]
+
+s="Sanyi 53"
+s_lista=s.split(" ")
+nevek.append(s_lista[0])
+korok.append(s_lista[1])
+print(s_lista)
+
