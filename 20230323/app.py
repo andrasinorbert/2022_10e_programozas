8348a4f97243b87576381e9ac6da693b8d04a234
andrasinorbert <andrasinorbert.puskasttt@gmail.com>
2023-03-24 12:31:58 +0100

    megjegyzés az adatbázis beállításokról

--- 20230323/app.py
+++ 20230323/app.py
@@ -1,6 +1,29 @@
 #SELECT * FROM `adatok`;
 #INSERT INTO `adatok`(`név`, `kor`, `magasság`) VALUES ('Julcsi', '13', '170');
 
+# Telepítettük a XAMPP alkalmazást (cross-platform)
+# elinditottuk az apache-ot és mysql-t
+#     ha valakinek hibát ir, hogy foglalt a port:
+#         config gomb, ini fájl szerkesztése
+#         a port számot mindenhol kicserüljük valami újra
+# megnyitottuk böngészőben a localhost/phpmyadmin felületet
+# bal oldalon készítettünk adatbázist (pl: 10e néven)
+# létrehoztunk egy táblát 4 oszloppal (pl: adatok néven)
+# megj.: az adatbázis név, táblanév ne tartalmazzon ékezetet, szóközt
+# az oszlopok:
+#     oszlopnév - tipus - egyéb
+#     id - int - A_I oszlop pipa
+#     név - varchar - maxhossz
+#     kor - int
+#     magasság - int
+# a táblára kattintva a felső menüsorban kiválasztottuk a "jogok" fület
+# új felhasználó létrehozása (szóközt ékezetet ne tartalmazzanak a mezők)
+#     név: 10e_user
+#     password: 10e_user (2 helyen kell megadni)
+#     Hely: legördülő menü -> helyi
+#     a többi dolgot békénhagytuk
+
+
 import pymysql
 # pip install pymysql
 
